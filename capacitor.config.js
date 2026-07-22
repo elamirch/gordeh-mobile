38fb9b6107da978c48f7b25c2db30b895edcd375
Amirreza <amirrezatm@protonmail.com>
2026-07-23 00:12:47 +0330

    Major improvements

---
+++ capacitor.config.js
@@ -0,0 +1,11 @@
+const config = {
+  appId: "com.gordeh.app",
+  appName: "گرده",
+  webDir: "www",
+  server: {
+    url: 'https://gordeh.com',
+    cleartext: false
+  }
+};
+
+module.exports = config;

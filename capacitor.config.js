982f770a4b570a241eca47a34472381c68860173
Amirreza <amirrezatm@protonmail.com>
2026-07-24 03:08:21 +0330

    Direct link opening back

--- capacitor.config.js
+++ capacitor.config.js
@@ -1,7 +1,13 @@
 const config = {
   appId: "com.gordeh.app",
   appName: "گرده",
-  webDir: "www"
+  webDir: "www",
+  appendUserAgent: "GordehNativeApp/1.0",
+  server: {
+    url: 'https://gordeh.com/panel',
+    cleartext: false,
+    androidScheme: 'https'
+  }
 };
 
 module.exports = config;
